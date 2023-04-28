1e342d54ded437458e8e704d4616d1e234f0bdb7
Rubeena-zt <rubeena.pk@zaeemsolutions.com>
2023-04-28 09:42:32 +0530

    initial commit

---
+++ my-project/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl font-bold bg-blue-800 underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
